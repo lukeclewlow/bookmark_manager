6744a9c633953abeed60740d058c5869b0b46f59
Luke Clewlow <luke.clewlow@gmail.com>
2015-01-13 15:00:09 +0000

    added more testing and the ability to add links

--- Gemfile
+++ Gemfile
@@ -2,7 +2,12 @@ source 'https://rubygems.org'
 
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'sinatra'	
 
 group :test do
-	gem 'rspec' 	
+	gem 'rspec'
+	gem 'database_cleaner'
+	gem 'capybara' 
+
 end
+
--- lib/server.rb
+++ lib/server.rb
@@ -1,18 +1,39 @@
 require 'data_mapper'
+require 'sinatra/base'
 
-env = ENV['RACK_ENV'] || 'development'
+class BookmarkManager < Sinatra::Base
 
-#we're telling datamapper to use a postgres database to localhost. 
-#The name will be "bookmark_manager_test" or "bookmark_manager_development"
-#depending on the environment
+	set :views, Proc.new { File.join(root, '..',"views") }
+	set :public_dir, Proc.new{File.join(root, '..', "public")}
+  set :public_folder, '/public'
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
-require './lib/link' #this needs to be done after datamapper is initialised
+	env = ENV['RACK_ENV'] || 'development'
 
-#after declaring your models you should finalize them
-DataMapper.finalize
+	#we're telling datamapper to use a postgres database to localhost. 
+	#The name will be "bookmark_manager_test" or "bookmark_manager_development"
+	#depending on the environment
 
-#However, the database tables don't exist yet. Let's tell datamapper to create them
-DataMapper.auto_upgrade!
+	DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
+	require './lib/link' #this needs to be done after datamapper is initialised
+
+	#after declaring your models you should finalize them
+	DataMapper.finalize
+
+	#However, the database tables don't exist yet. Let's tell datamapper to create them
+	DataMapper.auto_upgrade!
+
+	get '/' do
+		@links = Link.all		
+		erb :index
+	end
+
+	post '/links' do
+		url = params["url"]
+		title = params["title"]
+		Link.create(:url => url, :title => title)
+		redirect to('/')
+	end
+
+end
